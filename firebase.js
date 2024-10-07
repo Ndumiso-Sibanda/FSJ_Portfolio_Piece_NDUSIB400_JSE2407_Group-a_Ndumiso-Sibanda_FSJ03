c2cf62a777e9db5a609145c789e50b9093e4d8be
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-10-07 20:06:04 +0200

    installed firebase an dependecies

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1kkkhtOZFtDmHV3mvBkyYXVca84r2Uso",
+  authDomain: "e-commerce-d801d.firebaseapp.com",
+  projectId: "e-commerce-d801d",
+  storageBucket: "e-commerce-d801d.appspot.com",
+  messagingSenderId: "588612458836",
+  appId: "1:588612458836:web:0d00a2054bc92f201590c1"
+};
+
+const app = initializeApp(firebaseConfig);
