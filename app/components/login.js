59aa04d4823f85ac9b18c4eb77522875b3e51423
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-10-09 18:03:01 +0200

    implemented log in

---
+++ app/components/login.js
@@ -0,0 +1,4 @@
+export default function login() {
+<h1>login goes here</h1>
+
+}
---
+++ app/login/page.js
@@ -0,0 +1,69 @@
+'use client'
+
+import  { useState } from 'react';
+import {useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth'
+import {auth} from '@/app/firebase/firebase';
+import { useRouter } from 'next/navigation';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);4
+  const router = useRouter()
+
+
+  const handleSubmit = async () => {
+    try {
+    const res = await signInWithEmailAndPassword(email, password)
+    console.log ({res})
+    setEmail('');
+    setPassword('');
+    router.push('/products')
+    }catch (e) {
+    console.error(e)
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-indigo-600 shadow-md rounded-lg p-6 max-w-sm w-full">
+        <h2 className="text-white text-2xl mb-4 text-center">Log In</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-white mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full p-2 rounded-lg border border-gray-300"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-white mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full p-2 rounded-lg border border-gray-300"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-white text-indigo-600 font-bold py-2 rounded-lg hover:bg-gray-200 transition duration-200"
+          >
+            Log In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+export default Login;
